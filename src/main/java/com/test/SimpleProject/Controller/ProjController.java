9a896cb826d2852b1a38bb2ce69e06bb038b9f09
Arijit-Kumar-Pandab <arijitkumarpandab@gmail.com>
2024-10-22 16:05:20 +0530

    Initial commit

---
+++ src/main/java/com/test/SimpleProject/Controller/ProjController.java
@@ -0,0 +1,14 @@
+package com.test.SimpleProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello word";
+	}
+
+}
